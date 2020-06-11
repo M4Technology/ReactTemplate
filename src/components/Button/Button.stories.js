2388d2daa89f373b475971ade51e363c61bf995e
Vishal Chiluveri <vishalchiluveri@gmail.com>
2020-06-11 23:49:39 +0530

    Example upgraded

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
+import {action} from '@storybook/addon-actions';
+import {withKnobs, text} from '@storybook/addon-knobs/react';
+
 import Button from './';
 
 export default {
-    title: 'Button',
-    component: Button,
+    title: 'Design System|Button',
+    decorators: [withKnobs],
+    parameters: {
+        component: Button,
+        componentSubtitle: 'Displays an button',
+    },
 };
 
 export const Example = () => {
@@ -13,4 +19,20 @@ export const Example = () => {
             Example
         </Button>
     )
+};
+
+export const Example2 = () => {
+    return (
+        <Button onClick={action('button clicked')}>
+            Example2
+        </Button>
+    )
+};
+
+export const ExampleWithKnobs = () => {
+    return (
+        <Button onClick={action('button clicked')}>
+            {text('children', "hello")}
+        </Button>
+    );
 };
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import classNames from "classnames";
+import * as PropTypes from 'prop-types';
 
-export default function Button({children}) {
+/**
+ *- Use an avatar for attributing actions or content to specific users.
+ *- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
+ **/
+export default function Button({children, ...props}) {
     return (
         <button
+            {...props}
             className={classNames(
                 'border border-gray-500',
                 'p-1 px-3',
@@ -16,3 +22,14 @@ export default function Button({children}) {
         </button>
     );
 }
+
+Button.propTypes = {
+    /**
+     * Here the documentation goes of each prop
+     */
+    children: PropTypes.any,
+};
+
+Button.defaultProps = {
+    children: "Button",
+};
