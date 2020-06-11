6a679747a0c55c869323fab576f0b0daa3e4f578
Vishal Chiluveri <vishalchiluveri@gmail.com>
2020-06-11 23:49:13 +0530

    Updated storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
-    stories: ['../src/**/*.stories.(js|mdx)'],
+    stories: ['../**/*.stories.(js|mdx)', ],
     addons: [
         '@storybook/preset-create-react-app',
         '@storybook/addon-actions',
         '@storybook/addon-links',
         '@storybook/addon-knobs',
-        '@storybook/addon-docs'
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                configureJSX: true,
+            },
+        },
+        '@storybook/addon-viewport'
     ],
 };
