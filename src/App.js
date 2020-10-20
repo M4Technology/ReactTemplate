a6bca4473503dbbcb70b0bf4439c825e49fc74cc
Vishnu <vishnuviratkohli5@gmail.com>
2020-10-20 15:17:49 +0530

    removed not necessary components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import classNames from 'classnames';
-import Button from "./components/Button";
 
 function App() {
     return (
-        <div className={classNames(
-            'p-1'
-        )}>
-            <Button>
-                New Button
-            </Button>
+        <div>
+
         </div>
     );
 }
